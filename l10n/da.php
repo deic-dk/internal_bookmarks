8bb3d4255eec118698f6ce1ad15eeba4e8685ac6
deltafunction <frederik@orellana.dk>
2018-07-24 12:58:31 +0200

    Localization

---
+++ l10n/da.php
@@ -0,0 +1,8 @@
+<?php
+
+$TRANSLATIONS = Array(
+		'This bookmark points to a folder that no longer exists' =>
+			'Dette bogmærke peger på en folder der ikke længere findes',
+		'Would you like to delete it?' => 'Vil du slette det?',
+		'Confirm delete' => 'Bekræft sletning',
+		);
